6242ffba78c333591fe1d33f27328953d0e812bb
Freek Van der Herten <freek@spatie.be>
2023-01-13 23:37:41 +0100

    wip

--- tests/KeepsDeletedModelsTest.php
+++ tests/KeepsDeletedModelsTest.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Spatie\DeletedModels\Exceptions\NoModelFoundToRestore;
 use Spatie\DeletedModels\Models\DeletedModel;
 use Spatie\DeletedModels\Tests\TestSupport\Models\TestModel;
 use function Spatie\PestPluginTestTime\testTime;
@@ -119,3 +120,7 @@ it('can determine the attributes to be stored', function() {
 
     expect($deletedModel->value('name'))->toBe('John Doe suffix');
 });
+
+it('will throw an exception when trying to restore a not-existing model', function() {
+    TestModel::restore('non-existing');
+})->throws(NoModelFoundToRestore::class);
